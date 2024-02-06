9a61c38c3f595423ba8e9a089cbb354d79e85d56
darshanstudy <nhari51989@gmail.com>
2024-02-06 17:58:42 +0530

    second shell scripts

---
+++ 4variable.sh
@@ -0,0 +1,9 @@
+PERSON1=$1
+PERSON2=$2
+echo $PERSON1 : Hey $PERSON2! Is it you?
+
+echo $PERSON2 : Oh $PERSON1 How are you?
+
+echo $PERSON1 : I am fine, what about you $PERSON2
+
+echo $PERSON2 : Well $PERSON1
