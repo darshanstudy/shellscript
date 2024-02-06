fc156f726b3c88a9df515b0c48efb0f04127e6eb
darshanstudy <nhari51989@gmail.com>
2024-02-06 17:06:34 +0530

    second shell scripts

---
+++ 2variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Sample Conversation: Between Two Friends Who Meet in a Restaurant
+echo Rita : Hey Tina? Is it you?
+
+echo Tina : Oh Rita! How are you? It’s been a long time.
+
+echo Rita : I am fine, what about you? Yes, we last met during the board exams.
+
+echo Tina : Iam good too.
+
+echo Rita : What are you doing now?
+
+echo Tina : Well, I have started my undergraduate studies in English Honours at St. Xaviers College in Mumbai.
+
+
