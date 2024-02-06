02e268ea7f5fbe9b57b00b0fcded54a7df22200a
darshanstudy <nhari51989@gmail.com>
2024-02-06 17:33:36 +0530

    second shell scripts

--- 2variable.sh
+++ 2variable.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo Rita : Hey Tina? Is it you?
+PERSON1=Rita
+PERSON2=Tina
+echo $PERSON1 : Hey $PERSON2! Is it you?
 
-echo Tina : Oh Rita! How are you?
+echo $PERSON2 : Oh $PERSON1 How are you?
 
-echo Rita : I am fine, what about you Tina
+echo $PERSON1 : I am fine, what about you $PERSON2
 
-echo Tina : Well Rita
+echo Tina : Well $PERSON1
 
 
