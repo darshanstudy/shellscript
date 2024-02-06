a7bab1f46099f2cf1541c86efe8c42252c0d23b6
darshanstudy <nhari51989@gmail.com>
2024-02-06 18:24:42 +0530

    5th commit

--- 5variable.sh
+++ 5variable.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+ 
 USERNAME=$1
 PASSWORD=$2
 read USERNAME
 read PASSWORD
-echo username is :$USERNAME
-echo password is :$PASSWORD
+echo ENTER username is :$USERNAME
+echo ENTER password is :$PASSWORD
